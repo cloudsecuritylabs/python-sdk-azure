6d0f6ce458ad8ed5f4e2134d961dbb2e05f77fd6
Ankan Basu <basu.ankan@gmail.com>
2022-01-24 23:56:20 -0500

    Fixed spelling

--- resoucegroupmanager.py
+++ resoucegroupmanager.py
@@ -11,14 +11,14 @@ credential = AzureCliCredential()
 subscription_id = "3fa72fe6-7b7f-4556-9c96-f0287c8eae1a"
 
 # a resouce management client is needed to work with resource group and resources in Azure
-resouce_mgmt_client = ResourceManagementClient(credential, subscription_id)
+resource_mgmt_client = ResourceManagementClient(credential, subscription_id)
 
 # print the list of all resource group names with their locations
-resouce_group_list = resouce_mgmt_client.resource_groups.list()
-for count, item in enumerate(resouce_group_list):
+resource_group_list = resource_mgmt_client.resource_groups.list()
+for count, item in enumerate(resource_group_list):
     print(str(count+1) + ", " + item.name + ", " + item.location)
 
 # print all resources in a subscription with their name and location
-resource_list = resouce_mgmt_client.resources.list()
+resource_list = resource_mgmt_client.resources.list()
 for count, resource in enumerate(resource_list):
     print(str(count+1) + ", " + resource.name + ", " + resource.location + ", " + resource.type)
